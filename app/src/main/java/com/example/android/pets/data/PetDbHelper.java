d3db3df8217811250bd64fc6a3762263a7aa40dd
bergscott <berg.scott@ymail.com>
2017-02-07 12:00:49 -0600

    change db name to match tutorials

--- app/src/main/java/com/example/android/pets/data/PetDbHelper.java
+++ app/src/main/java/com/example/android/pets/data/PetDbHelper.java
@@ -15,7 +15,7 @@ import com.example.android.pets.data.PetContract.PetEntry;
 public class PetDbHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "pets.db";
+    public static final String DATABASE_NAME = "shelter.db";
 
     public PetDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
